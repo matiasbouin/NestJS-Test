c69ffa42aacbc54e37639c929e49a3368cc9f82b
matias bouin <matiasbouin@matiass-MBP.fibertel.com.ar>
2023-11-21 17:14:50 -0300

    finished crud

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,46 +1,66 @@
-import { Controller, Get, Post, Req} from '@nestjs/common';
+import { Controller, Get, Post, Req, Put, Delete} from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
+
     @Post()
     create(@Req() request: Request): string {
-        let munchiesBreed = new Breed()
-        munchiesBreed.name = 'nombreraza'
         let cat = new Cat()
-        cat.name = 'Munchies'
-        cat.breed = munchiesBreed
-        cat.age = 5
+        cat.id = request.body.id
+        cat.name = request.body.name
+        cat.color = request.body.color
+        cat.age = request.body.age
         cats.push(cat)
-        console.log('Request is: ', request.body)
         return 'Created one cat'
     }
 
     @Get()
     getAll(): string[] {
+        console.log('')
         return cats
     }
 
-    @Get('breeds')
-    getAllBreeds(): string {
-        return 'only one breed'
+    @Put()
+    modify(@Req() request: Request): string {
+        for (let i = 0; i < cats.length; i++) {
+            if(cats[i].id == request.body.id){
+                cats[i].name = request.body.name
+                cats[i].color = request.body.color
+                cats[i].age = request.body.age
+            }
+        }
+        return 'updated cat:' + request.body.id
     }
 
+    @Delete()
+    remove(@Req() request:Request): string{
+        for (let i = 0; i < cats.length; i++) {
+            if(cats[i].id == request.body.id){
+                cats.splice(i, 1)
+            }
+        }
+        return 'removed cat:' + request.body.id
+    }
 
 }
 
 export class Cat {
+    id: number
     name: string
-    breed: Breed
+    color: string
     age: number
     meow(){
         console.log('meow')
     }
 }
 
-export class Breed{
-    size: string
-    color: string
-    name: string
+type Request = {
+    body?: {
+        id?: number,
+        name?: string,
+        color?: string,
+        age?: number
+    }
 }
 
 let cats = [];
